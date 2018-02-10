68193c4782e009501585a8167e3002a0e6fc3a4e
Philip Chang <pchang211@gmail.com>
2018-02-10 01:03:45 -0800

    add autocomplete braces plugin

--- setup.sh
+++ setup.sh
@@ -22,6 +22,7 @@ vim_plugins() {
 	git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 	git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 	git clone https://github.com/terryma/vim-multiple-cursors ~/.vim/bundle/vim-multiple-cursors
+	git clone https://github.com/jiangmiao/auto-pairs
 }
 
 bash() {
