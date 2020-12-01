e46a57cd234118d93fa664fc61a51d9e5e6274a0
Philip Chang <pchang211@gmail.com>
2020-12-01 09:16:29 -0500

    cleanup zsh and reformat a bit

--- setup.sh
+++ setup.sh
@@ -2,44 +2,39 @@
 
 # fzf
 fzf() {
-	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-	yes | ~/.fzf/install
-	source ~/.zshrc
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    yes | ~/.fzf/install
+    source ~/.zshrc
 }
 
 pathogen() {
-	mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim	
-	echo "execute pathogen#infect()" >> ~/.vimrc
-	echo "syntax on" >> ~/.vimrc
-	echo "filetype plugin indent on" >> ~/.vimrc
+    mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim	
+    echo "execute pathogen#infect()" >> ~/.vimrc
+    echo "syntax on" >> ~/.vimrc
+    echo "filetype plugin indent on" >> ~/.vimrc
 }
 
 vim_plugins() {
-	mkdir -p ~/.vim/bundle
-	cd ~/.vim/bundle 
-	git clone https://github.com/tpope/vim-sensible.git ~/.vim/bundle/vim-sensible.git
-	git clone git://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
-	git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
-	git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-	git clone https://github.com/terryma/vim-multiple-cursors ~/.vim/bundle/vim-multiple-cursors
-	git clone https://github.com/jiangmiao/auto-pairs
-	git clone https://github.com/kien/ctrlp.vim
-	git clone https://github.com/myusuf3/numbers.vim.git
+    mkdir -p ~/.vim/bundle
+    cd ~/.vim/bundle 
+    git clone https://github.com/tpope/vim-sensible.git ~/.vim/bundle/vim-sensible.git
+    git clone git://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
+    git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+    git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+    git clone https://github.com/terryma/vim-multiple-cursors ~/.vim/bundle/vim-multiple-cursors
+    git clone https://github.com/jiangmiao/auto-pairs
+    git clone https://github.com/junegunn/fzf.vim
+    git clone https://github.com/myusuf3/numbers.vim.git
     git clone https://github.com/fatih/vim-go
     git clone https://github.com/tpope/vim-fugitive
     git clone https://github.com/myusuf3/numbers.vim.git ~/.vim/bundle/numbers
     git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
 }
 
-bash() {
-	cp .bashrc ~/
-	cp .bash_profile ~/
-}
-
 tmux() {
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	yes | brew install tmux
-	cp .tmux.conf ~/
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    yes | brew install tmux
+    cp .tmux.conf ~/
 }
 
 oh-my-zsh() {
@@ -63,12 +58,10 @@ then
 	tmux
 elif [ $1 == "all" ]
 then
-    oh-my-zsh
 	fzf
 	pathogen
 	vim_plugins
-	bash
 	tmux
 else
-	echo "Invalid target. Options: [fzf, pathogen, vim_plugins, bash, tmux, all]"
+	echo "Invalid target. Options: [fzf, pathogen, vim_plugins, tmux, all]"
 fi
